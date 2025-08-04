c2ffdfac13f2c016d2ec763e03e302812ecf4780
theodanielsjr101 <150858757+theodanielsjr101@users.noreply.github.com>
2025-08-04 17:01:37 +0000

    Grade Calculator

--- GradeCalculator/Program.cs
+++ GradeCalculator/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter numerical grade (0-100): ");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90) Console.WriteLine("Grade: A");
+        else if (grade >= 80) Console.WriteLine("Grade: B");
+        else if (grade >= 70) Console.WriteLine("Grade: C");
+        else if (grade >= 60) Console.WriteLine("Grade: D");
+        else Console.WriteLine("Grade: F");
+    }
+}
+
